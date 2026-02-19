7ab4d6463c98772648368603343ce719aa6bd2b9
midoridoriii <valissa069@gmail.com>
2026-02-19 13:45:52 +0200

    fix logger placeholder and improve error message

--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         Product macBook = new Product("MacBook Air 2020", BigDecimal.valueOf(1399));
         Product xiaomi = new Product("Xiaomi 12", BigDecimal.valueOf(499));
         List<Product> products = List.of(iphone, macBook, xiaomi);
-        logger.info("Data from DB was successfully fetched with user id {]", userId);
+        logger.info("Data from DB was successfully fetched with user id {}", userId);
         return products;
     }
 }
